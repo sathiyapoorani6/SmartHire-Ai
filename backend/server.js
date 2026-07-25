9eaadac88af51505e422c01688391f9b3c109ba2
sathiyapoorani6 <sathiyapoorani06@gmail.com>
2026-07-25 21:45:54 +0530

    Refactor server.js to use environment port

--- backend/server.js
+++ backend/server.js
@@ -70,6 +70,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
