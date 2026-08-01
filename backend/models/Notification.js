d1254c4c5423ead89c9b2429fbbc37c4a0a70333
sathiyapoorani6 <sathiyapoorani06@gmail.com>
2026-08-01 16:25:47 +0530

    Add Notification model with schema definition

---
+++ backend/models/Notification.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    jobTitle: {
+      type: String,
+      default: "",
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.models.Notification || mongoose.model("Notification", notificationSchema);
